17c7679159f8332138131fb761f9b0b5ee8bda7c
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-02-24 12:33:46 -0500

    commit v2 needs review

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -3,7 +3,12 @@ import spinner from './assets/spinner.gif'
 function Spinner() {
   return (
     <div className='w-100 mt-20'>
-      <img width={180} src={spinner} alt='Loading...' />
+      <img
+        className='text-center mx-auto'
+        width={180}
+        src={spinner}
+        alt='Loading...'
+      />
     </div>
   )
 }
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,29 +1,57 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setloading] = useState(true)
 
-  const fetchUsers = async () => {
-    console.log(GITHUB_URL)
-    const response = await fetch(`${GITHUB_URL}/users`, {
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
 
-    setUsers(data)
-    setloading(false)
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    })
   }
 
+  // Set loading
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   )
